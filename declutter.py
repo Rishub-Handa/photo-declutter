df27b6e4bc8c3b810d14064632b5cedde78c4bdd
Rishub Handa <rh4gh@virginia.edu>
2023-12-28 04:11:18 -0500

    added thumbnail sidebar

--- declutter.py
+++ declutter.py
@@ -10,6 +10,7 @@ import os
 import glob
 import shutil
 import tkinter as tk
+from tkinter import Frame, Canvas, Scrollbar
 from PIL import Image, ImageTk
 
 
@@ -36,12 +37,36 @@ class Declutter:
         self.curr_image_idx = 0
 
         # Setting up the Tkinter window
+        self.setup_ui()
+
+    def setup_ui(self):
+        # ... existing setup ...
         self.root = tk.Tk()
         self.root.title("Image Viewer")
-        self.label = tk.Label(self.root)
-        self.label.pack()
+        self.image_label = tk.Label(self.root)
+        self.image_label.pack(side="left")
         self.index_label = tk.Label(self.root, text="")
         self.index_label.pack()
+
+        # Create a scrollable frame for thumbnails
+        self.thumbnail_frame = Frame(self.root)
+        self.thumbnail_canvas = Canvas(self.thumbnail_frame)
+        self.scrollbar = Scrollbar(self.thumbnail_frame, orient="vertical", command=self.thumbnail_canvas.yview)
+        self.scrollable_frame = Frame(self.thumbnail_canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.thumbnail_canvas.configure(
+                scrollregion=self.thumbnail_canvas.bbox("all")
+            )
+        )
+
+        self.thumbnail_canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        self.thumbnail_canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.thumbnail_frame.pack(side="right", fill="y")
+        self.thumbnail_canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
 
         # Bind the key press event
         self.root.bind('<Right>', self.handle_key_press)
@@ -49,12 +74,36 @@ class Declutter:
         self.root.bind('<Up>', self.handle_key_press)
         self.root.bind('<Left>', self.handle_key_press)
 
+    def refresh_thumbnails(self):
+        # Clear current thumbnails
+        for widget in self.scrollable_frame.winfo_children():
+            widget.destroy()
+
+        # Load and display updated thumbnails
+        self.load_thumbnails()
+
+    def load_thumbnails(self):
+        # Load and display thumbnails for each image in the target directory
+        for image_path in os.listdir(self.target_dir):
+            if image_path.lower().endswith(('.jpg', '.jpeg', '.png', '.JPG', '.JPEG', '.PNG')):
+                img = Image.open(os.path.join(self.target_dir, image_path))
+                img.thumbnail((100, 100))  # Resize to thumbnail size
+                photo = ImageTk.PhotoImage(img)
+                label = tk.Label(self.scrollable_frame, image=photo)
+                label.image = photo  # Keep a reference!
+                label.pack()
+
     def run(self):
         # Display the first image
         self.update_image(0)
+        self.load_thumbnails()
 
         # Start the Tkinter event loop
         self.root.mainloop()
+
+    def update_window(self, image_idx): 
+        self.update_image(image_idx)
+        self.refresh_thumbnails()
 
     def update_image(self, image_idx):
         image_path = os.path.join(self.source_dir, self.image_files[image_idx])
@@ -67,8 +116,8 @@ class Declutter:
         img.thumbnail(max_size, Image.Resampling.LANCZOS)
 
         img = ImageTk.PhotoImage(img)
-        self.label.config(image=img)
-        self.label.image = img
+        self.image_label.config(image=img)
+        self.image_label.image = img
 
         dest = os.path.join(self.target_dir, os.path.basename(self.image_files[self.curr_image_idx]))
         checkmark = " ✅" if os.path.isfile(dest) else ""
@@ -84,15 +133,18 @@ class Declutter:
             dst = os.path.join(self.target_dir, os.path.basename(self.image_files[self.curr_image_idx]))
             shutil.copy(src, dst)
             print(f"Copied: {self.image_files[self.curr_image_idx]}")
+            self.refresh_thumbnails()
         elif event.keysym == 'Up':
             src = os.path.join(self.target_dir, os.path.basename(self.image_files[self.curr_image_idx]))
             if os.path.isfile(src):
                 os.remove(src)
                 print(f"Removed: {src}")
+                self.refresh_thumbnails()
             else:
                 print(f"File not found: {src}")
         elif event.keysym == 'Left':
             self.curr_image_idx = max(self.curr_image_idx - 1, 0)
+
 
         self.update_image(self.curr_image_idx)
 
